2220569261788a1814ca605f51e2f23ef159629f
G-Bence <gemesbence007@gmail.com>
2024-12-18 18:56:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_241213
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
