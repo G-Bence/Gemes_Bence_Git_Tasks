0d335f2f60f643fa6bc54106c0b7f552af92ecdc
G-Bence <gemesbence007@gmail.com>
2024-12-19 18:47:23 +0100

    F02 task completed

--- Program.cs
+++ Program.cs
@@ -9,15 +9,15 @@ namespace Class_241213
     internal class Program
     {
         /*
-            · Készíts egy „F01” nevű void típusú statikus metódust. A metódus várjon paraméterként egy egész számokból álló tömböt.
+            · Készíts egy „F02” nevű int típusú statikus metódust. A metódus várjon paraméterként egy egész számokból álló tömböt.
             
-            · Készíts egy Random típusú változót.
+            · Deklarálj egy egész szám típusú számláló változót.
             
-            · Készíts egy for ciklust, amely 0-tól a tömb hosszáig fut.
+            · Készíts egy for ciklust, amely a paraméterként várt tömb hosszáig fut. A cikluson belül döntsd el, hogy az adott indexen lévő töm eleme páros szám vagy sem. Ha igen, akkor a számláló változóhoz adj hozzá 1-et.
             
-            · A cikluson belül az aktuális indexű tömb eleméhez adj egész szám típusú értéket a randomizált változóval 1 és 50 között.
+            · A ciklus után return-öld a számláló változót.
             
-            · Készíts egy commit-ot „F01 feladat elkészítve” névvel, majd push-old is fel.
+            · Készíts egy commit-ot „F02 feladat elkészítve” névvel, majd push-old is fel.
         */
 
         static void F01(int[] array)
@@ -27,6 +27,21 @@ namespace Class_241213
             {
                 array[i] += random.Next(0, 51);
             }
+        }
+
+        static int F02(int[] array)
+        {
+            int counter = 0;
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
 
         static void Main(string[] args)
